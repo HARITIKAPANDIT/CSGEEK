d4c4618adba0896c5c39a45a905820a2960a85f1
Haritika Pandit <42149521+HARITIKAPANDIT@users.noreply.github.com>
2020-04-15 21:14:08 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#working with lists
+
+#making two lists
+student =[52,35,65,41,53]
+classes =["10A","12B","10B","11A","10C"]
+
+#using index() to get the index
+ind_11A = classes.index("11A")
+
+#printing the index
+ind_11A
+
+
+# In[5]:
+
+
+#printing the student no. of class 11A
+student[ind_11A]
+
+
+# In[7]:
+
+
+#working with dictionaries
+
+#creating a dictionary school
+school = {"10A" : 52, "12B" : 35, "10B" : 65, "11A" : 41, "10C" : 53}
+
+#printing student in class 11A
+school["11A"]
+
+
+# In[8]:
+
+
+#adding element to the existing dictionary
+school["12A"]=45
+
+#printing the list
+school
+
+
+# In[9]:
+
+
+#removing any element from list
+del(school["12A"])
+
+#printing the list
+school
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
